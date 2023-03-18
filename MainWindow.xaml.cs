ad0a3ab24b521fe35630f89e0ab636e11947b964
Etienne Eichenauer <etienne.eichenauer@students.hs-mainz.de>
2023-03-18 13:58:33 +0100

    bugfixes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Input;
 
 namespace RP_to_Eur
